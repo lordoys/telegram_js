f8fdd044592986e74ebaa7866ad770108ea850db
Pecherin Maxim <lordoys@gmail.com>
2021-05-10 23:53:16 +0300

    One more

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const express = require('express')
 const app = express()
-// const port = 8080
-const port = 3000
+const port = 8080
+// const port = 3000
 
 app.listen(port, () => {
     const { Telegraf } = require('telegraf')
     const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk')
+    const request = require('request');
+    const cheerio = require('cheerio');
 
     bot.start((ctx) => ctx.reply('ctx.message')) //ответ бота на команду /start
     bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
     bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
-    bot.hears('hi', (ctx) => {
-        let result = 'П';
-        let result1 = 'уст';
-        let result2 = 'о';
+    bot.use((ctx) => {
+        let result = 'OOOOO ';
+        let result1 = ' !!!!! ';
+        let result2 = ' @@@@@';
 
-        // fetch('https://www.appointfix.com/sunnyCourt/book/date')
-        //     .then(function (response) {
-        //         result =  response.status;
-        //     })
-        //     .then(function (template) {
-        //         result1 = template;
-        //     })
-        //     .catch(function (response) {
-        //         result2 = response.statusText;
-        //     });
+        request('https://www.toramp.com/schedule.php?id=' + ctx.message.text, function (error, response, body) {
+            if(error){console.log(error);}
+            else{
+                var $ = cheerio.load(body);
+                result = $('.title-basic span:first-child').text();
+            }
+            return ctx.reply(result)
+        });
 
-        return ctx.reply(result + ' ' + result1 + ' ' + result2)
-    }) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
+        // return ctx.reply(ctx.message.text)
+    })
     bot.launch()
 })
