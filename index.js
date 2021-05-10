d8201340e5cc51a9f0100fa8db61cf808209d2ad
Pecherin Maxim <lordoys@gmail.com>
2021-05-10 22:07:13 +0300

    One more

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk')
 
-bot.start((ctx) => ctx.reply('Welcome!!! ger')) //ответ бота на команду /start
+bot.start((ctx) => ctx.reply('Welcome!')) //ответ бота на команду /start
 bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
 bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
 bot.hears('hi', (ctx) => ctx.reply('Hey there')) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
