1abb0f67114cb25fbb698afacf32310a2d950de2
Pecherin Maxim <lordoys@gmail.com>
2021-05-10 21:59:39 +0300

    One more

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-// const { Telegraf } = require('telegraf')
-// const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk') //сюда помещается токен, который дал botFather
-// bot.start((ctx) => ctx.reply('Welcome!!!')) //ответ бота на команду /start
-// bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
-// bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
-// bot.hears('hi', (ctx) => ctx.reply('Hey there')) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
-// bot.launch() // запуск бота
 const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const { Telegraf } = require('telegraf')
+const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk')
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+bot.start((ctx) => ctx.reply('Welcome!!! ger')) //ответ бота на команду /start
+bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
+bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
+bot.hears('hi', (ctx) => ctx.reply('Hey there')) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
+bot.launch()
