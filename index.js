b3b8afe87fe52da531eeb705e45864ac0536b1b5
Pecherin Maxim <lordoys@gmail.com>
2021-05-10 23:11:38 +0300

    One more

--- index.js
+++ index.js
@@ -2,15 +2,31 @@ const express = require('express')
 const app = express()
 // const port = 8080
 const port = 3000
-const { Telegraf } = require('telegraf')
-const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk')
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    const { Telegraf } = require('telegraf')
+    const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk')
 
-bot.start((ctx) => ctx.reply('ctx.message')) //ответ бота на команду /start
-bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
-bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
-bot.hears('hi', (ctx) => ctx.reply('Hello, ' + ctx.message.from.first_name)) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
-bot.launch()
+    bot.start((ctx) => ctx.reply('ctx.message')) //ответ бота на команду /start
+    bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
+    bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
+    bot.hears('hi', (ctx) => {
+        let result = 'П';
+        let result1 = 'уст';
+        let result2 = 'о';
+
+        // fetch('https://www.appointfix.com/sunnyCourt/book/date')
+        //     .then(function (response) {
+        //         result =  response.status;
+        //     })
+        //     .then(function (template) {
+        //         result1 = template;
+        //     })
+        //     .catch(function (response) {
+        //         result2 = response.statusText;
+        //     });
+
+        return ctx.reply(result + ' ' + result1 + ' ' + result2)
+    }) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
+    bot.launch()
+})
