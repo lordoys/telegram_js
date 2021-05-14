1dfbda5fa31f819feff4dfc960a3dc0ccbb8be30
Pecherin Maxim <lordoys@gmail.com>
2021-05-15 00:53:16 +0300

    asdf

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 const express = require('express')
+const Nightmare = require('nightmare')
 const app = express()
 // const port = 8080
 const port = 3000
+const nightmare = Nightmare({ show: false })
 
 app.listen(port, () => {
     const { Telegraf } = require('telegraf')
     const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk')
-    const request = require('request');
-    const cheerio = require('cheerio');
 
-    bot.start((ctx) => ctx.reply('ctx.message')) //ответ бота на команду /start
+    // bot.start((ctx) => ctx.reply('ctx.message')) //ответ бота на команду /start
     bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
-    bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
-    bot.use((ctx) => {
-        let result = 'OOOOO ';
-        let result1 = ' !!!!! ';
-        let result2 = ' @@@@@';
+    bot.command('book', (ctx) => {
+        if(ctx.from.id === 237016450) {
+            let str = 'Привет, ' + ctx.message.from.first_name + '! Сейчас посмотрим какие есть свободные даты.';
+            ctx.reply(str);
+        }
 
-        request('https://www.toramp.com/schedule.php?id=' + ctx.message.text, function (error, response, body) {
-            if(error){console.log(error);}
-            else{
-                var $ = cheerio.load(body);
-                result = $('.title-basic span:first-child').text();
-            }
-            return ctx.reply(result)
-        });
+        nightmare
+            .goto('https://www.appointfix.com/sunnyCourt')
+            .wait(2000)
+            .wait('.business-info-screen-container-left-booking-button')
+            .click('.business-info-screen-container-left-booking-button')
+            .wait(2000)
+            .wait('.appointment-booking-services-select-details')
+            .click('.appointment-booking-services-list .appointment-booking-services-select.ng-star-inserted')
+            .wait(2000)
+            .click('.appointment-booking-services-select-submit-button')
+            .wait(2000)
+            .wait('.cal-days')
+            .evaluate(() => {
+                let result = [];
+                let allDays = document.querySelectorAll('.cal-day-cell');
+                allDays.forEach((item) => {
+                    if (item.classList.contains('cal-day-disabled')) {
 
-        // return ctx.reply(ctx.message.text)
+                    } else {
+                        result.push(item.querySelector('.cal-day-number').textContent);
+                    }
+                });
+                return result;
+            })
+            .then(result => {
+                console.log('Good');
+                ctx.reply(result);
+            })
+            .catch(error => {
+                console.error('Search failed:', error)
+            });
     })
     bot.launch()
 })
