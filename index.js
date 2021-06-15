c10257f6c52133080b30bc3b0c1fd7d0df4e7499
maksimpecerin <lordoys@gmail.com>
2021-06-15 18:50:41 +0300

    One more

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 const { Telegraf } = require('telegraf');
-// const bot = new Telegraf('1794222017:AAGieh5HVbGgzhXmAG8FUVClC-3_5YliUJU');
 const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk');
-// const Nightmare = require('nightmare')
-// const nightmare = Nightmare({ show: false })
 
 bot.start((ctx) => ctx.reply('Добро пожаловать!')); //ответ бота на команду /start
 bot.help((ctx) => ctx.reply('Тут нет.')); //ответ бота на команду /help
-// bot.hears('xxx', (ctx) => {
-//     nightmare
-//         .goto('https://learn.javascript.ru/async-await')
-//         .wait('.main__header-title')
-//         .evaluate(() => document.querySelector('.main__header-title').textContent)
-//         .end()
-//         .then(data => ctx.reply(data))
-//         .catch(error => {
-//             console.error('Search failed:', error)
-//             ctx.reply(error)
-//         });
-//     ctx.reply('asdfg');
-// })
 
 bot.launch();
