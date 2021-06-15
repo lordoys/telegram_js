45e6619a72fd3111f1c2566c14e3b96018cdc9e7
Pecherin Maxim <lordoys@gmail.com>
2021-06-15 20:09:36 +0300

    One more

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const express = require('express');
 const port = 8080;
 const { Telegraf } = require('telegraf');
 const app = express();
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({ show: false });
 const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk');
 
 app.listen(port, () => {
-    bot.start((ctx) => ctx.reply('Добро пожаловать!')); //ответ бота на команду /start
-    bot.help((ctx) => ctx.reply('Тут sdfsdfsdfsdfsfdsfd нет.')); //ответ бота на команду /help
+    bot.start((ctx) => ctx.reply('Добро пожаловать!'));
+    bot.hears('xxx', async (ctx) => {
+        const response = await nightmare
+            .goto('https://learn.javascript.ru/async-await')
+            .wait('.main__header-title')
+            .evaluate(() => document.querySelector('.main__header-title').textContent)
+            .end()
+            .then()
+            .catch(error => {
+                console.error('Search failed:', error)
+            });
+
+        ctx.reply(response);
+    })
+    bot.help((ctx) => ctx.reply('Тут sdfsdfsdfsdfsfdsfd нет.'));
 
     bot.launch();
 });
