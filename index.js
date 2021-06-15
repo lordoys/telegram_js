d668b996588bea07f7205922ce01b19b5123cf75
Pecherin Maxim <lordoys@gmail.com>
2021-06-15 19:48:08 +0300

    One more

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk');
 
 bot.start((ctx) => ctx.reply('Добро пожаловать!'));
 bot.help((ctx) => ctx.reply('Тут ничего нет, совсем.'));
-bot.launch();
+bot.launch().then(r => console.log(r));
