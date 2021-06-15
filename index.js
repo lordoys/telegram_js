1e31043a8ad55db2cbcd21322ef4ed40af75e5ba
Pecherin Maxim <lordoys@gmail.com>
2021-06-15 20:22:10 +0300

    One more

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk');
 
 app.listen(port, () => {
     bot.start((ctx) => ctx.reply('Добро пожаловать!'));
-    bot.hears('xxx', async (ctx) => {
-        const response = await nightmare
+    bot.help((ctx) => ctx.reply('Тут sdfsdfsdfsdfsfdsfd нет.'));
+    bot.hears('xxx',  (ctx) => {
+        nightmare
             .goto('https://learn.javascript.ru/async-await')
             .wait('.main__header-title')
             .evaluate(() => document.querySelector('.main__header-title').textContent)
             .end()
-            .then()
+            .then(response => ctx.reply(response))
             .catch(error => {
                 console.error('Search failed:', error)
             });
 
-        ctx.reply(response);
+
     })
-    bot.help((ctx) => ctx.reply('Тут sdfsdfsdfsdfsfdsfd нет.'));
 
     bot.launch();
 });
--- test.js
+++ test.js
@@ -10,20 +10,16 @@ const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk');
 app.listen(port, () => {
     bot.start((ctx) => ctx.reply('Добро пожаловать!')); //ответ бота на команду /start
     bot.help((ctx) => ctx.reply('Тут нет.')); //ответ бота на команду /help
-    bot.hears('xxx', async (ctx) => {
-        const response = await nightmare
+    bot.hears('xxx',  (ctx) => {
+        nightmare
             .goto('https://learn.javascript.ru/async-await')
             .wait('.main__header-title')
             .evaluate(() => document.querySelector('.main__header-title').textContent)
             .end()
-            .then()
+            .then(data => ctx.reply(data))
             .catch(error => {
                 console.error('Search failed:', error)
             });
-
-        ctx.reply(response);
     })
-
-
     bot.launch();
 });
