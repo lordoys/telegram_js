6ae56890a47cfeb58a91c8dce52e063d8eba444b
Pecherin Maxim <lordoys@gmail.com>
2021-06-15 19:52:32 +0300

    One more

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const express = require('express');
+const port = 8080;
 const { Telegraf } = require('telegraf');
+const app = express();
 const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk');
 
-bot.start((ctx) => ctx.reply('Добро пожаловать!'));
-bot.help((ctx) => ctx.reply('Тут ничего нет, совсем.'));
-bot.launch().then(r => console.log(r));
+app.listen(port, () => {
+    bot.start((ctx) => ctx.reply('Добро пожаловать!')); //ответ бота на команду /start
+    bot.help((ctx) => ctx.reply('Тут sdfsdfsdfsdfsfdsfd нет.')); //ответ бота на команду /help
+
+    bot.launch();
+});
--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const bot = new Telegraf('1731362068:AAGZGBDhdv7h5-jBCps1ZyTPuq8ZPXD0Ztk');
 
 app.listen(port, () => {
     bot.start((ctx) => ctx.reply('Добро пожаловать!')); //ответ бота на команду /start
-    bot.help((ctx) => ctx.reply('Тут d a нет.')); //ответ бота на команду /help
+    bot.help((ctx) => ctx.reply('Тут нет.')); //ответ бота на команду /help
 
     bot.launch();
 });
